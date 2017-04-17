b95424f5e1935fe60ab0d731f5c331163fdf5f57
Quinton Weenink <quinton.weenink@standardbank.co.za>
2017-04-17 17:47:34 +0200

    Basic working solution

--- quartermaster.py
+++ quartermaster.py
@@ -14,10 +14,10 @@ class Quartermaster:
     def __init__(self, crewSize):
         self.s = {}
         self.host = socket.gethostname()  # Get local machine name
-        self.port = 12396  # Reserve a port for your service.
+        self.port = 12347  # Reserve a port for your service.
         self.crewSize = crewSize
         self.clueList = []
-        self.verifyListSize = 79
+        self.verifyListSize = 199
         self.mapListSize = 20.0
         self.clueListSize = 1000.0
 
@@ -35,74 +35,6 @@ class Quartermaster:
         self.s.listen(20)  # Now wait for client connection.
 
         self.captain = Rummy(crewSize)
-
-    def toString(self):
-        self.captain.printCrew()
-
-    def addToClueList(self, id, data):
-        clue = {"id": id, "data": [data]}
-        self.clueList.append(clue)
-
-
-    def isDone(self):
-        for member in self.captain.crew:
-            if len(member.clues) > 0:
-                return False
-        return True
-
-    def isAllDead(self):
-        for member in self.captain.crew:
-            if len(member.clues) > 0 or member.paused == False:
-                return False
-        return True
-
-
-    def backspace(self, n):
-        print('\r'*n)
-
-    def printPirates(self, m, i, mc):
-        output = "+-----------------------------------------------------------------------+\n"
-        output += "| ID\t\t\t\t\t| Conn\t| Solv\t| Left\t| Fail\t|\n"
-        output += "|-----------------------------------------------------------------------|\n"
-        for pirate in self.captain.crew:
-            output += pirate.toString()
-        output += "+-----------------------------------------------------------------------+\n"
-
-        clueSetPercentage = ((float(i) / float(self.verifyListSize)) * 100.0)
-        clueSetDisplay = int(clueSetPercentage / 2.0)
-
-        mapPercentage = ((float(mc) / float(self.clueListSize)) * 100.0)
-        mapDisplay = int(mapPercentage / 2.0)
-
-        problemPercentage = (((float(m) / float(self.mapListSize)) * 100.0)+(mapPercentage/self.mapListSize))
-        problemDisplay = int(problemPercentage / 2.0)
-
-        output += "Problem completion (~): \n"
-        output += str("{0:.1f}".format((problemPercentage if (problemPercentage < 100.0) else 100.0))) + " %\t["
-        for x in range(0, problemDisplay):
-            output += u'\u2588'
-        for x in range(0, 50 - problemDisplay):
-            output += " "
-        output += "]\n"
-
-        output += "Map completion (~):\n"
-        output += str("{0:.1f}".format((mapPercentage if (mapPercentage < 100.0) else 100.0))) + " %\t["
-        for x in range(0, mapDisplay):
-            output += u'\u2588'
-        for x in range(0, 50 - mapDisplay):
-            output += " "
-        output += "]\n"
-
-        output += "Clue set completion:\n"
-        output += str("{0:.1f}".format((clueSetPercentage if (clueSetPercentage < 100.0) else 100.0))) + " %\t["
-        for x in range(0, clueSetDisplay):
-            output += u'\u2588'
-        for x in range(0, 50 - clueSetDisplay):
-            output += " "
-        output += "]\n"
-        return output
-
-
 
     def listenDispatch(self):
 
@@ -207,8 +139,76 @@ class Quartermaster:
             numMap += 1
         self.s.close()
 
+    def toString(self):
+        self.captain.printCrew()
 
-quartermaster = Quartermaster(5)
+    def addToClueList(self, id, data):
+        clue = {"id": id, "data": [data]}
+        self.clueList.append(clue)
+
+
+    def isDone(self):
+        for member in self.captain.crew:
+            if len(member.clues) > 0:
+                return False
+        return True
+
+    def isAllDead(self):
+        for member in self.captain.crew:
+            if len(member.clues) > 0 or member.paused == False:
+                return False
+        return True
+
+
+    def backspace(self, n):
+        print('\r'*n)
+
+    def printPirates(self, m, i, mc):
+        output = "+-----------------------------------------------------------------------+\n"
+        output += "| ID\t\t\t\t\t| Conn\t| Solv\t| Left\t| Fail\t|\n"
+        output += "|-----------------------------------------------------------------------|\n"
+        for pirate in self.captain.crew:
+            output += pirate.toString()
+        output += "+-----------------------------------------------------------------------+\n"
+
+        clueSetPercentage = ((float(i) / float(self.verifyListSize)) * 100.0)
+        clueSetDisplay = int(clueSetPercentage / 2.0)
+
+        mapPercentage = ((float(mc) / float(self.clueListSize)) * 100.0)
+        mapDisplay = int(mapPercentage / 2.0)
+
+        problemPercentage = (((float(m) / float(self.mapListSize)) * 100.0)+(mapPercentage/self.mapListSize))
+        problemDisplay = int(problemPercentage / 2.0)
+
+        output += "Problem completion (~): \n"
+        output += str("{0:.1f}".format((problemPercentage if (problemPercentage < 100.0) else 100.0))) + " %\t["
+        for x in range(0, problemDisplay):
+            output += u'\u2588'
+        for x in range(0, 50 - problemDisplay):
+            output += " "
+        output += "]\n"
+
+        output += "Map completion (~):\n"
+        output += str("{0:.1f}".format((mapPercentage if (mapPercentage < 100.0) else 100.0))) + " %\t["
+        for x in range(0, mapDisplay):
+            output += u'\u2588'
+        for x in range(0, 50 - mapDisplay):
+            output += " "
+        output += "]\n"
+
+        output += "Clue set completion:\n"
+        output += str("{0:.1f}".format((clueSetPercentage if (clueSetPercentage < 100.0) else 100.0))) + " %\t["
+        for x in range(0, clueSetDisplay):
+            output += u'\u2588'
+        for x in range(0, 50 - clueSetDisplay):
+            output += " "
+        output += "]\n"
+        return output
+
+
+
+
+
+
+quartermaster = Quartermaster(10)
 quartermaster.listenDispatch()
-
-
